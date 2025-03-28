0ddea89a4ca298c323030cbb1555eac91df63487
Romain-dev85 <r.auvinet@gmail.com>
2025-03-28 11:14:16 +0100

    pointer on to share icon ant script for share icon

--- scripts/list-creation.js
+++ scripts/list-creation.js
@@ -1,10 +1,20 @@
-const myList = document.querySelector("#my-list");
-const newLi = document.createElement("li");
+// const myList = document.querySelector("#my-list");
+// const newLi = document.createElement("li");
 
 
-const newInput = document.createElement("input");
-newInput.type = "text";
-newInput.placeholder = "Ajouter une tâche";
+// const newInput = document.createElement("input");
+// newInput.type = "text";
+// newInput.placeholder = "Ajouter une tâche";
 
-newLi.appendChild(newInput);
-myList.appendChild(newLi);
+// newLi.appendChild(newInput);
+// myList.appendChild(newLi);
+
+const shareButton = document.getElementById("share-list");
+
+shareButton.addEventListener("click", share)
+
+function share() {
+    alert("Veuillez vous connecter avant de pouvoir partager");
+}
+
+
