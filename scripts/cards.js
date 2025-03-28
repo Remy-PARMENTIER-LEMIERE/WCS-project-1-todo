51fa8e8f9f4ef5a02ba2251e285962e6476da882
Remy-PARMENTIER-LEMIERE <remyparmentierlemiere@hotmail.fr>
2025-03-28 10:25:56 +0100

    script for cards management done, created a link in the menu tocreate lists, corrected css onto about page and some adjustement into main-application css and html

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,3 +1,5 @@
+//VARIABLES
+
 const originalCard = document.querySelector("#original-card");
 const buttonsCreateCard = document.querySelectorAll(".create-button");
 const cardsContainer = document.querySelector("main");
@@ -22,6 +24,8 @@ buttonsCreateCard.forEach((button) => {
     menu.classList.remove("open-menu");
   });
 });
+
+// NEW TITLE, NEW TODOS & NEW MENU-CARD ON TITLE CREATION
 
 const menuCardsContainer = document.querySelector(".carroussel");
 
@@ -49,20 +53,42 @@ cardsContainer.addEventListener("submit", (e) => {
   } else {
     const newLi = document.createElement("li");
     newLi.innerHTML = `${input.value}<span>
-                            <img class="check-todo"
-                                src="../assets/img/check.png"
-                                alt="Valider la todo">
-                            <img class="checked-todo"
-                                src="../assets/img/checked.png"
-                                alt="La todo est validée">
-                            <img class="delete-todo"
-                                src="../assets/img/white-cross.png"
-                                alt="Supprimer la ToDo">
-                        </span>`;
+    <img class="check-todo"
+    src="../assets/img/check.png"
+    alt="Valider la todo">
+    <img class="checked-todo"
+    src="../assets/img/checked.png"
+    alt="La todo est validée">
+    <img class="delete-todo"
+    src="../assets/img/white-cross.png"
+    alt="Supprimer la ToDo">
+    </span>`;
     todos.appendChild(newLi);
     input.value = "";
   }
 });
+
+// STYLE ADJUSTMENT ON CARROUSSEL RELATIVE TO SCREEN WIDTH
+
+const menuCardsQuantity = Array.from(
+  document.querySelector(".carroussel").children
+).length;
+
+if (window.innerWidth < 992) {
+  if (menuCardsQuantity <= 2) {
+    menuCardsContainer.style.justifyContent = "center";
+    menuCardsContainer.style.gap = "2rem";
+  }
+} else {
+  if (menuCardsQuantity <= 4) {
+    menuCardsContainer.style.justifyContent = "center";
+    menuCardsContainer.style.gap = "2rem";
+  }
+}
+
+// DELETE, CHECK & UNCHECK EACH TODO
+// ALSO DELETE CARD AND MENU CARD ON CARD CROSS CLICK
+const centeredButton = document.querySelector("main > button");
 
 cardsContainer.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete-todo")) {
@@ -73,31 +99,89 @@ cardsContainer.addEventListener("click", (e) => {
   } else if (e.target.classList.contains("checked-todo")) {
     e.target.style.display = "none";
     e.target.previousElementSibling.style.display = "inline-block";
+  } else if (e.target.classList.contains("delete-full-list")) {
+    const cardBellonging = e.target.parentNode;
+    const linkedMenuCardClassName = ".menu-card-" + cardBellonging.id.slice(5);
+    const linkedMenuCard = document.querySelector(linkedMenuCardClassName);
+
+    cardBellonging.classList.remove("focused-card");
+
+    if (cardBellonging.previousElementSibling.id !== "original-card") {
+      cardBellonging.previousElementSibling.classList.add("focused-card");
+    } else if (cardBellonging.nextElementSibling !== null) {
+      cardBellonging.nextElementSibling.classList.add("focused-card");
+    } else {
+      centeredButton.style.display = "inline-block";
+    }
+
+    cardBellonging.remove();
+    linkedMenuCard.remove();
   }
 });
 
+// CHANGE THE FOCUSED CARD AND FOCUSED MENU CARD
+
 menuCardsContainer.addEventListener("click", (e) => {
-  const targetClasses = e.target.classList;
-  const classNameStart = "menu-card-";
+  const targetClasses = e.target.className;
+  const targetParentClasses = e.target.parentNode.className;
+  const classNameStartWith = "menu-card-";
+  const actualFocusedMenuCard = document.querySelector(".focused-menu-card");
+  // console.log(targetClasses);
 
-  const classListContains = () => {
-    for (let i = 0; i < targetClasses.length; i++) {
-      if (targetClasses[i].startsWith(classNameStart)) {
-        return true;
-      }
-    }
-  };
-
-  if (classListContains()) {
+  if (
+    targetClasses.includes("focused-menu-card") ||
+    targetParentClasses.includes("focused-menu-card")
+  ) {
     const classArray = e.target.className.split(" ");
     const cardNumber = classArray[0].slice(10);
-    const cardToPrint = "card-" + cardNumber;
+    const idCardToPrint = "card-" + cardNumber;
 
     const actualFocus = document.querySelector(".focused-card");
-    const focusToDo = document.getElementById(cardToPrint);
+    const focusToDo = document.getElementById(idCardToPrint);
 
     actualFocus.classList.remove("focused-card");
     focusToDo.classList.add("focused-card");
     menu.classList.remove("open-menu");
+  } else if (
+    targetClasses.startsWith(classNameStartWith) &&
+    targetClasses.includes("todo-list")
+  ) {
+    if (actualFocusedMenuCard !== null) {
+      actualFocusedMenuCard.classList.remove("focused-menu-card");
+    }
+    e.target.classList.add("focused-menu-card");
+  } else if (targetClasses.startsWith(classNameStartWith)) {
+    if (actualFocusedMenuCard !== null) {
+      actualFocusedMenuCard.classList.remove("focused-menu-card");
+    }
+    e.target.parentNode.classList.add("focused-menu-card");
+  }
+});
+
+// DELETE CARD AND MENU CARD WITH MENU BUTTON
+
+const deleteButton = document.getElementById("delete-button");
+
+deleteButton.addEventListener("click", () => {
+  const focusedMenuCard = document.querySelector(".focused-menu-card");
+  if (focusedMenuCard !== null) {
+    const classArray = focusedMenuCard.className.split(" ");
+    const cardNumber = classArray[0].slice(10);
+    const idCardToDelete = "card-" + cardNumber;
+    const cardToDelete = document.getElementById(idCardToDelete);
+    const actualFocusedCard = document.querySelector(".focused-card");
+
+    if (actualFocusedCard === cardToDelete) {
+      if (cardToDelete.previousElementSibling.id !== "original-card") {
+        cardToDelete.previousElementSibling.classList.add("focused-card");
+      } else if (cardToDelete.nextElementSibling !== null) {
+        cardToDelete.nextElementSibling.classList.add("focused-card");
+      } else {
+        centeredButton.style.display = "inline-block";
+      }
+    }
+
+    cardToDelete.remove();
+    focusedMenuCard.remove();
   }
 });
